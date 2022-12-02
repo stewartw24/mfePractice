f1cb7201940e6b42d1ca60aacd752f64d0e00535
William <stewart-w24@hotmail.co.uk>
2022-12-02 15:11:44 +0000

    fix for typo in yml file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,13 +3,10 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
-    test,
-    (
-      <div>
-        <h1>Hi there</h1>
-        <hr />
-        <MarketingApp />
-      </div>
-    )
+    <div>
+      <h1>Hi there</h1>
+      <hr />
+      <MarketingApp />
+    </div>
   );
 };
