284af86ed4427d69d561ff690957f18ce084ae5e
William <stewart-w24@hotmail.co.uk>
2022-12-05 12:46:54 +0000

    added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,11 +7,12 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "marketing",
-      filename: "remoteeEntry.js",
+      filename: "remoteEntry.js",
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
